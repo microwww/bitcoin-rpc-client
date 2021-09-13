7387db5e9e2ca6b3d14b82f07ea97b9d76594854
changshu.li <lcs.005@163.com>
2021-09-13 17:48:17 +0800

    version 0.1.0 / &lt;

--- src/main/java/com/github/microwww/bitcoin/api/WalletApi.java
+++ src/main/java/com/github/microwww/bitcoin/api/WalletApi.java
@@ -268,32 +268,31 @@ public class WalletApi extends JsonRpcClient {
      * 1. requests     (array, required) Data to be imported
      *   [     (array of json objects)
      *     {
-     *       "scriptPubKey": "<script>" | { "address":"<address>" }, (string / json, required) Type of scriptPubKey (string for script, json for address)
+     *       "scriptPubKey": "&lt;script&gt;" | { "address":"&lt;address&gt;" }, (string / json, required) Type of scriptPubKey (string for script, json for address)
      *       "timestamp": timestamp | "now"                        , (integer / string, required) Creation time of the key in seconds since epoch (Jan 1 1970 GMT),
      *                                                               or the string "now" to substitute the current synced blockchain time. The timestamp of the oldest
      *                                                               key will determine how far back blockchain rescans need to begin for missing wallet transactions.
      *                                                               "now" can be specified to bypass scanning, for keys which are known to never have been used, and
      *                                                               0 can be specified to scan the entire blockchain. Blocks up to 2 hours before the earliest key
      *                                                               creation time of all keys being imported by the importmulti call will be scanned.
-     *       "redeemscript": "<script>"                            , (string, optional) Allowed only if the scriptPubKey is a P2SH address or a P2SH scriptPubKey
-     *       "pubkeys": ["<pubKey>", ... ]                         , (array, optional) Array of strings giving pubkeys that must occur in the output or redeemscript
-     *       "keys": ["<key>", ... ]                               , (array, optional) Array of strings giving private keys whose corresponding public keys must occur in the output or redeemscript
-     *       "internal": <true>                                    , (boolean, optional, default: false) Stating whether matching outputs should be treated as not incoming payments
-     *       "watchonly": <true>                                   , (boolean, optional, default: false) Stating whether matching outputs should be considered watched even when they're not spendable, only allowed if keys are empty
-     *       "label": <label>                                      , (string, optional, default: '') Label to assign to the address (aka account name, for now), only allowed with internal=false
+     *       "redeemscript": "&lt;script&gt;"                            , (string, optional) Allowed only if the scriptPubKey is a P2SH address or a P2SH scriptPubKey
+     *       "pubkeys": ["&lt;pubKey&gt;", ... ]                         , (array, optional) Array of strings giving pubkeys that must occur in the output or redeemscript
+     *       "keys": ["&lt;key&gt;", ... ]                               , (array, optional) Array of strings giving private keys whose corresponding public keys must occur in the output or redeemscript
+     *       "internal": &lt;true&gt;                                    , (boolean, optional, default: false) Stating whether matching outputs should be treated as not incoming payments
+     *       "watchonly": &lt;true&gt;                                   , (boolean, optional, default: false) Stating whether matching outputs should be considered watched even when they're not spendable, only allowed if keys are empty
+     *       "label": &lt;label&gt;                                      , (string, optional, default: '') Label to assign to the address (aka account name, for now), only allowed with internal=false
      *     }
      *   ,...
      *   ]
      * 2. options                 (json, optional)
      *   {
-     *      "rescan": <false>,         (boolean, optional, default: true) Stating if should rescan the blockchain after all imports
+     *      "rescan": &lt;false&gt;,         (boolean, optional, default: true) Stating if should rescan the blockchain after all imports
      *   }
      * </pre>
      *
      * @param requests
      */
     @NoComplete
-    // importmulti "requests" ( "options" )
     public void importmulti(String requests) {
     }
 
